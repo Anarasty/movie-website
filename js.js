54226dfcdde7d38a03ec0da26a31ed39a9222238
Anarasty <murloc.ninja16@gmail.com>
2022-04-05 19:57:21 +0300

    search bar

--- js.js
+++ js.js
@@ -5,6 +5,8 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&"+API_KEY;
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
+const searchURL = BASE_URL + '/search/movie?' + API_KEY;
+
 // const API_KEY = "api_key=1cf50e6248dc270629e802686245c2c8";
 // const BASE_URL = "https://api.themoviedb.org/3";
 // const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
@@ -12,6 +14,8 @@ const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
 
 const main = document.getElementById('main');
+const searchForm = document.getElementById('searchForm');
+const searchField= document.getElementById('searchField');
 
 
 getData(API_URL);
@@ -44,3 +48,13 @@ function showMovie(data) {
       main.appendChild(movieElement);
     })
 }   
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = searchField.value;
+
+    if(searchTerm) {
+        getData(searchURL+"&query="+searchTerm)
+    }
+})
